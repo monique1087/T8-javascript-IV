29794b7a74074cf370e889aaf99c320b80d0aa89
moniquealmeida <monique10879@gmail.com>
2019-11-05 13:23:09 -0300

    nadie

---
+++ objects/revisao/index.js
@@ -0,0 +1,13 @@
+const pessoa = new Object
+nome = "Monique",
+idade = "200";
+
+function Professor(nome, sobrenome, materia) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.materia = materia
+}
+
+// Professor.prototype.dizMateria = function {
+
+// }
